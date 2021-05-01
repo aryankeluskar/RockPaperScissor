246c53289dc04f547b928856b856c431b22486fa
Aryan K <aryan.keluskar@icloud.com>
2021-05-01 18:21:04 +0530

    modified:   script.js 	modified:   styleL.css 	modified:   styleM.css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (screen.availWidth > screen.availHeight) {
 } else {
     console.log("phone");
     document.write("<link rel=\"stylesheet\" href=\"styleM.css\" >")
-    document.getElementById('comsi').innerHTML = "Phone "
+    document.getElementById('comsi').innerHTML = "Phone"
 }
 
 if (10 > 5) {
@@ -18,6 +18,7 @@ let cr = Math.floor((Math.random() * 300000) / 100000) + 1;
 console.log(cr);
 
 function stnclick() {
+    document.getElementById('ussi').innerHTML = "You: Rock";
     document.getElementById('user').src = "rock.png"
     document.getElementById('worl').style.visibility = "visible"
     if (cr == 1) {
@@ -26,6 +27,7 @@ function stnclick() {
             // document.getElementById('worl').style.border = "2.5vw solid #ffd700"
         document.getElementById('worl').style.borderColor = "#ffd700"
         document.getElementById('com').src = "rock.png"
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Rock ";
     }
     if (cr == 2) {
         document.getElementById('worl').innerHTML = "lost"
@@ -33,12 +35,14 @@ function stnclick() {
             // document.getElementById('worl').style.border = "2.5vw solid #f00"
         document.getElementById('worl').style.borderColor = "#f00"
         document.getElementById('com').src = "paper.png"
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Paper";
     }
     if (cr == 3) {
         document.getElementById('worl').innerHTML = "won"
         document.getElementById('worl').style.color = "#0f0"
         document.getElementById('worl').style.borderColor = "#0f0"
         document.getElementById('com').src = "scissor.png"
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Rock ";
     }
     document.getElementById("btnplay1").disabled = "true";
     document.getElementById("btnplay2").disabled = "true";
@@ -47,6 +51,7 @@ function stnclick() {
 }
 
 function papclick() {
+    document.getElementById('ussi').innerHTML = "You: Paper";
     document.getElementById('user').src = "paper.png"
     document.getElementById('worl').style.visibility = "visible"
     if (cr == 2) {
@@ -55,6 +60,8 @@ function papclick() {
             // document.getElementById('worl').style.border = "2.5vw solid #ffd700"
         document.getElementById('worl').style.borderColor = "#ffd700"
         document.getElementById('com').src = "paper.png"
+            // document.getElementById('comsi').innerHTML = "Paper";
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Paper";
     }
     if (cr == 3) {
         document.getElementById('worl').innerHTML = "lost"
@@ -62,13 +69,17 @@ function papclick() {
             // document.getElementById('worl').style.border = "2.5vw solid #f00"
         document.getElementById('worl').style.borderColor = "#f00"
         document.getElementById('com').src = "scissor.png"
+            // document.getElementById('comsi').innerHTML = "Scissor";
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Scissor ";
     }
     if (cr == 1) {
+        document.getElementById('comsi').innerHTML = "Rock";
         document.getElementById('worl').innerHTML = "won"
         document.getElementById('worl').style.color = "#0f0"
             // document.getElementById('worl').style.border = "2.5vw solid #0f0"
         document.getElementById('worl').style.borderColor = "#0f0"
         document.getElementById('com').src = "rock.png"
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Rock ";
     }
     document.getElementById("btnplay1").disabled = "true";
     document.getElementById("btnplay2").disabled = "true";
@@ -77,6 +88,7 @@ function papclick() {
 }
 
 function sciclick() {
+    document.getElementById('ussi').innerHTML = "You: Scissor";
     document.getElementById('user').src = "scissor.png"
     document.getElementById('worl').style.visibility = "visible"
     if (cr == 3) {
@@ -84,20 +96,26 @@ function sciclick() {
         document.getElementById('worl').style.color = "#ffd700"
             // document.getElementById('worl').style.border = "2.5vw solid #ffd700"
         document.getElementById('worl').style.borderColor = "#ffd700"
+            // document.getElementById('comsi').innerHTML = "Scissor";
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Scissor";
         document.getElementById('com').src = "scissor.png"
     }
     if (cr == 1) {
+        // document.getElementById('comsi').innerHTML = "Rock";
         document.getElementById('worl').innerHTML = "lost"
         document.getElementById('worl').style.color = "f00"
             // document.getElementById('worl').style.border = "2.5vw solid #f00"
         document.getElementById('worl').style.borderColor = "#f00"
         document.getElementById('com').src = "rock.png"
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Rock";
     }
     if (cr == 2) {
         document.getElementById('worl').innerHTML = "won"
         document.getElementById('worl').style.color = "#0f0"
             // document.getElementById('worl').style.border = "2.5vw solid #0f0"
         document.getElementById('worl').style.borderColor = "#0f0"
+            // document.getElementById('comsi').innerHTML = "Paper";
+        document.getElementById('comsi').innerHTML = document.getElementById('comsi').innerHTML + ": Paper";
         document.getElementById('com').src = "paper.png"
     }
     document.getElementById("btnplay1").disabled = "true";
